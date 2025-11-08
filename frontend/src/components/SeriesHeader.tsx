0a55c3bd7f68cc77e14f6626c1c44f5da3988ce6
sandy <sandy6yang@gmail.com>
2025-11-08 17:12:11 -0500

    add series header

---
+++ frontend/src/components/SeriesHeader.tsx
@@ -0,0 +1,11 @@
+import { HeaderSimple } from "../components/Header"; // reuse your existing header component
+import {SPATHS} from "../constants/SeriesNavigation"
+
+export default function SeiresHeader() {
+    const seriesLinks = SPATHS.map((item) => ({
+    ...item,
+    link: `/profile${item.link}`,
+  }));
+
+  return <HeaderSimple links={seriesLinks} />;
+}
---
+++ frontend/src/constants/SeriesNavigation.tsx
@@ -0,0 +1,27 @@
+import Skullpanda from "../pages/Skullpanda";
+import Hirono from "../pages/Hirono";
+import Labubu from "../pages/Labubu";
+
+export const BACKEND_BASE_PATH = 'https://fa23-lec9-demo-soln.fly.dev/api';
+
+export const SPATHS: {
+    link: string;
+    label: string;
+    element?: JSX.Element;
+}[] = [
+    {
+        link: "/skullpanda",
+        label: "Skullpanda",
+        element: <Skullpanda />,
+    },
+        {
+        link: "/hirono",
+        label: "Hirono",
+        element: <Hirono />,
+    },
+        {
+        link: "/labubu",
+        label: "Labubu",
+        element: <Labubu />,
+    },
+];
