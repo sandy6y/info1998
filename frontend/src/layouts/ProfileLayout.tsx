556af0418813e3bcd617f7bef24711825beabd51
sandy <sandy6yang@gmail.com>
2025-11-08 17:13:25 -0500

    change profile layout

---
+++ frontend/src/layouts/ProfileLayout.tsx
@@ -0,0 +1,20 @@
+import { Outlet } from "react-router-dom";
+import SeriesHeader from "../components/SeriesHeader";
+import Profile from "../pages/Profile";
+
+export default function ProfileLayout() {
+  return (
+    <main style={{ textAlign: "center" }}>
+      <h1>Blind Box Library</h1>
+      <h2>Profile</h2>
+    
+      <Profile />
+    
+      <SeriesHeader />
+
+      <section style={{ marginTop: "20px" }}>
+        <Outlet />
+      </section>
+    </main>
+  );
+}
---
+++ frontend/src/pages/Profile.tsx
@@ -0,0 +1,8 @@
+export default function Profile() {
+  return (
+    <div style={{ textAlign: "center" }}>
+      <p>Username:</p>
+      <p>Total figures:</p>
+    </div>
+  );
+}
