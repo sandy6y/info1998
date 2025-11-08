df3867be16517bb98a1091c40b4e7d1e33f33c63
sandy <sandy6yang@gmail.com>
2025-11-08 14:28:22 -0500

    add logo to page

--- frontend/src/layouts/RootLayout.tsx
+++ frontend/src/layouts/RootLayout.tsx
@@ -1,9 +1,13 @@
 import { HeaderSimple } from "../components/Header";
 import { PATHS } from "../constants/Navigation";
 import { Outlet } from "react-router-dom";
+import logo from "../images/POPLLECTION.jpg";
 
 const RootLayout = () => (
     <div>
+        <h1>
+            <img src = {logo} alt="Logo" className="logo" />
+        </h1>
         <HeaderSimple links={PATHS} />
         <div>
             <Outlet />
