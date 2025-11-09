5a1d06124e368e906cecf6ad297ed0a5f40412fa
sandy <sandy6yang@gmail.com>
2025-11-08 21:48:28 -0500

    images for skullpanda warmth seires

--- frontend/src/pages/Home.tsx
+++ frontend/src/pages/Home.tsx
@@ -1,7 +1,7 @@
 const HomePage = () => (
     <body>
         <center>
-            <h1>Welcome</h1>
+            <h1>Welcome!</h1>
             <p>A website to showcase all the popmart blind box figures you have collected!</p>
         </center>
     </body>
